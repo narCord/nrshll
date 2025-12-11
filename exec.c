9c09980ae17153e349340e11b789d6b7e3a59052
Narciso <narcordeiro@gmail.com>
2025-12-11 23:41:33 +0100

    Añadida estructura inicial

--- exec.c
+++ exec.c
@@ -3,4 +3,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void determine_type(const char *cmd){
+    if (strcmp(cmd, "cd")){
 
+    }
+    else if (strcmp(cmd, "exit")){
+
+    }
+    else if (strcmp(cmd, "echo")){
+
+    }
+    else if (strcmp(cmd, "pwd")){
+
+    }
+    else if (strcmp(cmd, "help")){
+
+    }
+    else if (strcmp(cmd, "alias")){
+
+    }
+    else{
+
+    }
+}
