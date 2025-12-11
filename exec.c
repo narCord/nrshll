9db9842e95593eb44faa73620209f7a0e3294a68
Narciso <narcordeiro@gmail.com>
2025-12-12 00:31:55 +0100

    Blah

--- exec.c
+++ exec.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void determine_type(const char *cmd){
+// true = interno,  false = externo
+bool determine_command_type(const char *cmd){
     if (strcmp(cmd, "cd")){
-
+        return true;
     }
     else if (strcmp(cmd, "exit")){
-
+        return true;
     }
     else if (strcmp(cmd, "echo")){
-
+        return true; 
     }
     else if (strcmp(cmd, "pwd")){
-
+        return true; 
     }
     else if (strcmp(cmd, "help")){
-
+        return true; 
     }
     else if (strcmp(cmd, "alias")){
-
+         return true;
     }
     else{
-
+        return false; 
     }
+}
+
+void execute_external(){
+    
 }
